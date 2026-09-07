08cc5b76cc169aa22a27f567d945139017f3e9ad
Venkateswara VP <venkateswara.vp@gmail.com>
2026-09-07 08:21:52 -0400

    Added coverage report

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,12 @@ export default defineConfig({
   test: {
     setupFiles: ['./test/setup.ts'],
     globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['html', 'lcov', 'text'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['src/index.ts'],
+    },
   },
 });
